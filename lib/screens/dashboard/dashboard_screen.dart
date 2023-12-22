6d6fbd55f28d16a2717737db387f837d0b0b2626
Luis <luis.guaman@fullsegura.com>
2023-12-22 17:20:09 -0500

    3d gltf viewer

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:admin/responsive.dart';
 import 'package:admin/screens/dashboard/components/chart.dart';
 import 'package:admin/screens/dashboard/components/my_fields.dart';
 import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 import '../../constants.dart';
 import 'components/ChartHist.dart';
@@ -35,6 +36,20 @@ class DashboardScreen extends StatelessWidget {
                       if (Responsive.isMobile(context))
                         SizedBox(height: defaultPadding),
                       if (Responsive.isMobile(context)) StorageDetails(),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 400,
+                        child: const ModelViewer(
+//                          backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+                        backgroundColor: Colors.transparent,
+                          src: 'https://dashboard.twintivo.com/assets/images/wh1.glb',
+                          alt: 'A Twintivo 3D Viewer',
+                          ar: true,
+                          autoRotate: true,
+                          iosSrc: 'https://modelviewer.dev/shared-assets/models/Astronaut.usdz',
+                          disableZoom: false,
+                        ),
+                      ),
                     ],
                   ),
                 ),
